d33aec775b9b9a87482ee4c11308b0a819959d93
Nate Moore <nate@skypack.dev>
2021-12-15 14:26:17 -0600

    chore: scaffold out site

---
+++ astro.config.ts
@@ -0,0 +1,11 @@
+import type { AstroUserConfig } from "astro";
+
+const config: AstroUserConfig = {
+  buildOptions: {
+    site: 'https://astro.build',
+    sitemap: true,
+  },
+  renderers: [],
+}
+
+export default config;
