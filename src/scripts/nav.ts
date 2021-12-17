ac6856328536d3902e12301bd1f4ea91365d65b8
Nate Moore <nate@skypack.dev>
2021-12-17 08:44:00 -0600

    fix: font sizing, improved nav behavior

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -2,7 +2,7 @@ const nav = document.querySelector('nav.main') as HTMLElement;
 
 const io = new IntersectionObserver(
   ([e]) => e.target.toggleAttribute('stuck', e.intersectionRatio < 1),
-  {threshold: [1]}
+  { threshold: 1 }
 )
 
 io.observe(nav)
