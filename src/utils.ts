6a1d296ab049a62e181f56998c180dd2d6604c2d
Nate Moore <nate@skypack.dev>
2021-12-21 15:41:21 -0600

    feat: wrap up design pass

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
-let stars: number;
+const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+
+let stars: string;
 export async function getStars() {
     if (stars) return stars;
-    stars = await fetch('https://api.github.com/repos/withastro/astro').then(res => res.json()).then(res => res.stargazers_count);
+    stars = await fetch('https://api.github.com/repos/withastro/astro').then(res => res.json()).then(res => formatter.format(res.stargazers_count));
     return stars;
 }
