6c84add4a34cff8817dd68c483cda89a3033cd3f
Nate Moore <nate@skypack.dev>
2021-12-25 14:45:43 -0600

    feat: no 🥚 w/ prefers-reduced-motion

--- src/scripts/defer.js
+++ src/scripts/defer.js
@@ -1,3 +1,3 @@
-if (window.matchMedia('(hover: hover)').matches) {
+if (window.matchMedia('(hover: hover) and and (prefers-reduced-motion: no-preference)').matches) {
     import('/src/scripts/analytics.js').then(mod => mod.lazy());
 }
