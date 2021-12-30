327235b7e4b8728600979ac37ffd1b183f73498e
Nate Moore <nate@skypack.dev>
2021-12-29 22:12:52 -0600

    feat: add blog posts

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,30 @@ import type { AstroUserConfig } from "astro";
 
 const config: AstroUserConfig = {
   buildOptions: {
-    site: 'https://astro.build',
+    site: "https://astro.build",
     sitemap: true,
   },
   renderers: [],
+  markdownOptions: {
+    render: [
+      "@astrojs/markdown-remark",
+      {
+        remarkPlugins: [
+          "remark-smartypants",
+          ["remark-autolink-headings", { behavior: "wrap" }],
+        ],
+        rehypePlugins: [
+          "rehype-slug",
+          ["rehype-autolink-headings", { behavior: "wrap" }],
+        ],
+      },
+    ],
+  },
   vite: {
-    ssr: { 
-      external: ['svgo']
-    }
-  }
-}
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+};
 
 export default config;
