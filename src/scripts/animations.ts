079912014cf84f542fbdd7b3b2ef85deee95c9e7
Nate Moore <nate@skypack.dev>
2022-01-07 20:11:06 -0600

    feat: improve animation triggers

--- src/scripts/animations.ts
+++ src/scripts/animations.ts
@@ -1,14 +1,41 @@
+const timeouts = new Map();
+const triggered = new Set();
+const DELAY = 1000;
+function trigger(el: HTMLElement) {
+    const key = el.id;
+    if (triggered.has(key)) {
+        return;
+    }
+    clear(el);
+    timeouts.set(key, setTimeout(() => {
+        el.style.setProperty('--animation-play-state', 'running');
+        triggered.add(key);
+        io.unobserve(el);
+        timeouts.delete(key);
+    }, DELAY));
+}
+function clear(el: HTMLElement) {
+    const key = el.id;
+    if (timeouts.has(key)) {
+        const handle = timeouts.get(key);
+        clearTimeout(handle);
+        timeouts.delete(key);
+    }
+}
 const io = new IntersectionObserver((entries) => {
     for (const entry of entries) {  
         const el = entry.target as HTMLElement;
         if (entry.isIntersecting && entry.intersectionRatio > 0.9) {
-            el.style.setProperty('--animation-play-state', 'running');
-            io.unobserve(entry.target);
+            trigger(el);
         } else {
+            clear(el);
             el.style.setProperty('--animation-play-state', 'paused');
         }
     }
 }, { threshold: [0, 0.5, 1] });
 
 const illustrations = Array.from(document.querySelectorAll('.illustration'));
-illustrations.forEach(illo => io.observe(illo));
+illustrations.forEach(illo => {
+    io.observe(illo);
+    (illo as HTMLElement).style.setProperty('--animation-play-state', 'paused');
+});
