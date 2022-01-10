964a02f4e25d4ceea158387731c1a79db481ddbe
Nate Moore <nate@skypack.dev>
2022-01-10 15:21:26 -0600

    audit: fix safari animation loop

--- src/scripts/animations.ts
+++ src/scripts/animations.ts
@@ -1,39 +1,3 @@
-const timeouts = new Map();
-const triggered = new Set();
-const DELAY = 1000;
-function trigger(el: HTMLElement) {
-    const key = el.id;
-    if (triggered.has(key)) {
-        return;
-    }
-    clear(el);
-    timeouts.set(key, setTimeout(() => {
-        el.style.setProperty('--animation-play-state', 'running');
-        triggered.add(key);
-        io.unobserve(el);
-        timeouts.delete(key);
-    }, DELAY));
-}
-function clear(el: HTMLElement) {
-    const key = el.id;
-    if (timeouts.has(key)) {
-        const handle = timeouts.get(key);
-        clearTimeout(handle);
-        timeouts.delete(key);
-    }
-}
-const io = new IntersectionObserver((entries) => {
-    for (const entry of entries) {  
-        const el = entry.target as HTMLElement;
-        if (entry.isIntersecting && entry.intersectionRatio > 0.9) {
-            trigger(el);
-        } else {
-            clear(el);
-            el.style.setProperty('--animation-play-state', 'paused');
-        }
-    }
-}, { threshold: [0, 0.5, 1] });
-
 async function loop(element: Element) {
     const animations = element.getAnimations({ subtree: true });
     await Promise.all(animations.map(anim => anim.finished));
@@ -46,16 +10,12 @@ async function loop(element: Element) {
 }
 
 function setup() {
-    timeouts.clear();
-    triggered.clear();
     const illustrations = Array.from(document.querySelectorAll('.illustration'));
-    illustrations.forEach(illo => io.observe(illo));
-
     const hydrate = illustrations.find(el => (el as HTMLElement).dataset.name === 'hydration');
     loop(hydrate);
 }
 setup();
+
 window.addEventListener('astro:navchange', () => {
-    console.log('astro:navchange')
     setup();
 });
