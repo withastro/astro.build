f92facdd9a2717528696389a9bb52aabdc3cabbd
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-03-17 18:15:14 +0100

    WIP: searching NPM from a script rather than every build

--- src/data/integrations.ts
+++ src/data/integrations.ts
@@ -1,63 +1,26 @@
-import { contains } from "../utils/contains";
-import { uniq } from "../utils/uniq";
+import integrations from './integrations.json';
 
-interface IntegrationData {
-    title: string;
-    description: string;
-    image: string;
-    categories: string[];
-    repoUrl: string;
-    npmUrl: string;
-    url?: string;
-}
+function getCollections() {
+    const collectionsMap = new Map<string, number>();
 
-let _loadIntegrations: Promise<App.Integration[]>;
-async function loadIntegrations(): Promise<App.Integration[]> {
-    const items = import.meta.globEager<{ [slug: string]: IntegrationData }>('./integrations/*.json');
-
-    return Object.keys(items).map(slug => {
-        const integration = items[slug].default;
-
-        return {
-            ...integration,
-            slug,
-            image: integration.image && {
-                src: integration.image,
-                alt: integration.description,
-            },
-            repoUrl: {
-                href: integration.repoUrl,
-                text: integration.title,
-            },
-            npmUrl: {
-                href: integration.npmUrl,
-                text: integration.title,
-            },
-            url: {
-                href: integration.url || integration.npmUrl || integration.repoUrl,
-                text: integration.title,
+    for (const integration of integrations) {
+        for (const category of integration.categories) {
+            if (collectionsMap.has(category)) {
+                collectionsMap.set(category, collectionsMap.get(category) + 1);
+            } else {
+                collectionsMap.set(category, 1);
             }
         }
-    })
+    }
+
+    return Array.from(collectionsMap.entries())
+        .map(([category, count]) => ({ category, count }))
+        .sort((a, b) => b.count === a.count ? a.category.localeCompare(b.category) : b.count - a.count)
+        .map(({ category }) => category.replace('+', ' + '));
 }
 
-export async function fetchIntegrations() {
-    _loadIntegrations = _loadIntegrations || loadIntegrations();
-    return _loadIntegrations;
-}
+export const collections = getCollections();
 
-export async function fetchIntegrationsForCollection(collection: string) {
-    const allIntegrations = await fetchIntegrations();
-
-    const containsCollection = contains(collection);
-
-    return allIntegrations.filter(integration => containsCollection(integration.categories))
-}
-
-export async function fetchCollections(): Promise<string[]> {
-    const integrations = await fetchIntegrations();
-
-    return uniq(
-        integrations.map(integration => integration.categories).flat()
-    );
+export function getIntegrationsForCollection(collection: string) {
+    return integrations.filter(({ categories }) => categories.indexOf(collection) >= 0);
 }
