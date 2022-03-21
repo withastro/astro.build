12edf3a93698d087981ce2faffe14299cde54eb1
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-03-21 14:24:36 +0100

    WIP: refactoring global navigation data

--- src/components/FlyoutMenu.ts
+++ src/components/FlyoutMenu.ts
@@ -1,49 +1,11 @@
-export class FlyoutMenu extends HTMLElement {
-    #hoverOpened = false;
-
-    connectedCallback() {
-        this.button.addEventListener('click', () => this.toggle());
-        this.button.addEventListener('mouseenter', () => this.onMouseEnter());
-        this.button.addEventListener('mouseleave', () => this.onMouseLeave());
+function flyout(elem: HTMLDetailsElement) {
+    elem.onmouseenter = () => {
+        elem.setAttribute('open', 'open');
     }
-
-    get button(): HTMLButtonElement {
-        return this.querySelector('button[type="button"]');
-    }
-
-    get content() {
-        return this.querySelector('section');
-    }
-
-    get open() {
-        return this.hasAttribute('open');
-    }
-
-    set open(value: boolean) {
-        if (value) {
-            this.setAttribute('open', '');
-            this.content.setAttribute('aria-hidden', 'false');
-        } else {
-            this.removeAttribute('open');
-            this.content.setAttribute('aria-hidden', 'true');
-        }
-    }
-
-    toggle() {
-        this.open = !this.open;
-    }
-
-    onMouseEnter() {
-        if (this.open) { return; }
-
-        this.#hoverOpened = true;
-        this.open = true;
-    }
-
-    onMouseLeave() {
-        if (!this.open || !this.#hoverOpened) { return; }
-        this.open = false;
-    }
+    elem.onmouseleave = () => {
+        elem.removeAttribute('open');
+    };
 }
 
-customElements.define('flyout-menu', FlyoutMenu);
+Array.from(document.querySelectorAll<HTMLDetailsElement>('details.flyout'))
+    .map(flyout);
