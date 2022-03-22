c629098a1818692b8e49584e3b648852ba12af32
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-03-22 12:43:24 +0100

    adding line-clamp and removing some of the integration overrides

--- tailwind.config.js
+++ tailwind.config.js
@@ -139,6 +139,7 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
     function ({ addComponents, theme }) {
       addComponents({
         ".container": {
