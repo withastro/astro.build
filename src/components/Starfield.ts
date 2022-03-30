695acf4d40c9d287547290c8e1295169b00ce8b0
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-03-30 14:29:30 +0200

    using an observer on the starfield animation as well

---
+++ src/components/Starfield.ts
@@ -0,0 +1,19 @@
+class StarField extends HTMLElement {
+    #io: IntersectionObserver
+
+    connectedCallback() {
+        if ('IntersectionObserver' in window) {
+            this.#io = new IntersectionObserver(([entry]) => {
+                this.visible = entry.isIntersecting
+            })
+
+            this.#io.observe(this)
+        }
+    }
+
+    set visible(value: boolean) {
+        this.setAttribute('visible', `${value}`)
+    }
+}
+
+customElements.define('star-field', StarField)
