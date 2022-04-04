475bd2e0b21f1adc0dde7f69d1b02eb8d5b11001
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-04-04 16:50:51 +0200

    use fetch for caching API, just feature detect first

--- src/scripts/preload.ts
+++ src/scripts/preload.ts
@@ -1,27 +1,27 @@
-const links = document.querySelectorAll<HTMLAnchorElement>('a')
+if (typeof fetch === 'function') {
+    const links = document.querySelectorAll<HTMLAnchorElement>('a')
 
-function preload(href: string) {
-    const xhr = new XMLHttpRequest()
-    xhr.open("GET", href)
-    xhr.send()
-}
-
-function onPreload(event: MouseEvent) {
-    const target = event.target as HTMLAnchorElement;
-
-    if (!target.href) {
-        return;
+    function preload(href: string) {
+        fetch(href, { cache: "force-cache" });
     }
 
-    target.removeEventListener('mouseenter', onPreload, true);
-    target.removeEventListener('touchstart', onPreload, true);
+    function onPreload(event: MouseEvent) {
+        const target = event.target as HTMLAnchorElement;
 
-    preload(target.href);
-}
+        if (!target.href) {
+            return;
+        }
 
-for (const link of links) {
-    if (link.href.startsWith(location.origin)) {
-        link.addEventListener('mouseenter', onPreload, true);
-        link.addEventListener('touchstart', onPreload, true);
+        target.removeEventListener('mouseenter', onPreload, true);
+        target.removeEventListener('touchstart', onPreload, true);
+
+        preload(target.href);
     }
+
+    for (const link of links) {
+        if (link.href.startsWith(location.origin)) {
+            link.addEventListener('mouseenter', onPreload, true);
+            link.addEventListener('touchstart', onPreload, true);
+        }
+}
 }
