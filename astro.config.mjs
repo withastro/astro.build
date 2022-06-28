2f41394e18deb3c029684f8ae6de429217e100d6
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-06-28 16:45:46 +0000

    Adds @astrojs/prefetch and updates to the latest beta (#170)
    * Adding the new @astrojs/prefetch integration
    
    * linter format fixes
    
    * Revert "linter format fixes"
    
    This reverts commit 36d193a5471823be9984b032b81d17884b7ca61c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,31 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import { tokens } from './syntax-highlighting-theme.mjs'
+import { defineConfig } from 'astro/config';
+import prefetch from "@astrojs/prefetch";
+import sitemap from '@astrojs/sitemap';
+import { tokens } from './syntax-highlighting-theme.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro.build',
-	integrations: [sitemap()],
-	markdown: {
-		shikiConfig: {
-			theme: {
-				name: 'Star gazer',
-				type: 'dark',
-				settings: tokens,
-			},
-		},
-		remarkPlugins: [
-			'remark-smartypants',
-			[
-				'remark-autolink-headings',
-				{
-					behavior: 'wrap',
-				},
-			],
-		],
-		rehypePlugins: [
-			'rehype-slug',
-			[
-				'rehype-autolink-headings',
-				{
-					behavior: 'wrap',
-				},
-			],
-		],
-	},
-	vite: {
-		ssr: {
-			noExternal: ['smartypants'],
-			external: ['svgo', '@11ty/eleventy-img'],
-		},
-	},
-})
+  site: 'https://astro.build',
+  integrations: [sitemap(), prefetch()],
+  markdown: {
+    shikiConfig: {
+      theme: {
+        name: 'Star gazer',
+        type: 'dark',
+        settings: tokens
+      }
+    },
+    remarkPlugins: ['remark-smartypants', ['remark-autolink-headings', {
+      behavior: 'wrap'
+    }]],
+    rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
+      behavior: 'wrap'
+    }]]
+  },
+  vite: {
+    ssr: {
+      noExternal: ['smartypants'],
+      external: ['svgo', '@11ty/eleventy-img']
+    }
+  }
+});
