72ce03591845ea2617837f50df7ecf4ba1c36b18
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-07-07 20:46:58 +0000

    Migrating to @astrojs/image (#188)
    * Adds @astrojs/image to the Showcase
    
    * WIP: Fixing showcase images in SSG build
    
    * Adding a helper to centralize local image loading for catalogs
    
    * adding @astrojs/image for themes catalog
    
    * using @astrojs/image for sponsor avatars
    
    * removing sharp and astro-eleventy-img deps :tada:
    
    * ensuring the picture's image always has height and width
    
    * removing new sponsor image
    
    * removing old copy:images script
    
    * temp: adding loading and decoding in the local <Picture />
    
    * cleaning up a few more .ts file imports
    
    * adding alt tags to the responsive images
    
    * adding alt text to showcase images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 import prefetch from "@astrojs/prefetch";
 import sitemap from '@astrojs/sitemap';
 import { tokens } from './syntax-highlighting-theme.mjs';
@@ -6,7 +7,7 @@ import { tokens } from './syntax-highlighting-theme.mjs';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro.build',
-  integrations: [sitemap(), prefetch()],
+  integrations: [sitemap(), prefetch(), image()],
   markdown: {
     shikiConfig: {
       theme: {
--- scripts/update-sponsors.mjs
+++ scripts/update-sponsors.mjs
@@ -1,6 +1,4 @@
 import fs from 'node:fs'
-import fetch from 'node-fetch'
-import sharp from 'sharp'
 import { fetchAllMembers } from './opencollective.mjs'
 
 const skipTier = new Set(['Platinum Sponsor', 'Gold Sponsor'])
@@ -22,39 +20,11 @@ function normalizeMember(member) {
 	}
 }
 
-async function cacheAvatar(member) {
-	if (!member.image) {
-		return member
-	}
-
-	const inputBuffer = await fetch(member.image).then(res => res.buffer())
-
-	if (!fs.existsSync('public/assets/sponsors')) {
-		fs.mkdirSync('public/assets/sponsors', { recursive: true })
-	}
-
-	const output = `public/assets/sponsors/${member.id}.jpg`
-
-	await sharp(inputBuffer)
-		.resize(128)
-		.jpeg({ mozjpeg: true })
-		.toFile(output, (error, info) => {
-			if (error) {
-				console.error(error)
-			}
-		})
-
-	return {
-		...member,
-		image: output.replace('public', ''),
-	}
-}
-
 async function main() {
 	const allMembers = await fetchAllMembers()
 
 	const members = await Promise.all(
-		allMembers.filter(isUser).map(normalizeMember).map(cacheAvatar)
+		allMembers.filter(isUser).map(normalizeMember)
 	)
 
 	fs.writeFileSync(
