9b22ffb6b8ba5fba0b2ee7e3eeb40b878831a699
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-07-14 14:47:59 -0500

    support prioritizing collection order, hide empty groups when searching

--- src/data/integrations.ts
+++ src/data/integrations.ts
@@ -1,5 +1,12 @@
 import integrations from './integrations.json'
 
+// collections in this list will always be sorted first
+const COLLECTION_PRIORITY = {
+	'featured': 1,
+	'official': 2,
+	'other': -1 // always pushed to the end
+}
+
 export interface Collection {
 	text: string
 	slug: string
@@ -20,11 +27,22 @@ function getCollections(): Collection[] {
 
 	return Array.from(collectionsMap.entries())
 		.map(([category, count]) => ({ category, count }))
-		.sort((a, b) =>
-			b.count === a.count
-				? a.category.localeCompare(b.category)
-				: b.count - a.count
-		)
+		.sort((a, b) => {
+			const aPriority = COLLECTION_PRIORITY[a.category]
+			const bPriority = COLLECTION_PRIORITY[b.category]
+
+			if (aPriority >= 0 && bPriority >=0) {
+				return aPriority - bPriority
+			} else if (aPriority >= 0 || bPriority === -1) {
+				return -1
+			} else if (bPriority >= 0 || aPriority === -1) {
+				return 1
+			} else if (a.count !== b.count) {
+				return b.count - a.count
+			} else {
+				return a.category.localeCompare(b.category)
+			}
+		})
 		.map(({ category }) => ({
 			slug: category,
 			text: category.replace('+', ' + '),
