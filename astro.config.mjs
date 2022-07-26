8c9ca3d22f72016ccc1693fe631854f19e82b2fd
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-07-26 18:05:12 +0000

    Updating to the RC :tada: (#212)
    * updating to the RC :tada:
    
    * link color tweaks
    
    * fixing about and legal page styles
    
    * one last link style fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import { tokens } from './syntax-highlighting-theme.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+  legacy: {
+    astroFlavoredMarkdown: true,
+  },
   site: 'https://astro.build',
   integrations: [sitemap(), prefetch(), image()],
   markdown: {
