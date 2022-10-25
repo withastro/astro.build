82f44547b36d738330caf9edb08378f305b9856e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-10-25 10:58:48 -0500

    new themecard style

---
+++ src/components/styles.ts
@@ -0,0 +1,5 @@
+import clsx from 'clsx'
+
+export const neonOutlineCardStyle = clsx(
+    'border border-transparent first:decoration-transparent border-primary-300 shadow shadow-sky-200 rounded-lg hover:border-primary-400 hover:shadow-accent-300'
+)
