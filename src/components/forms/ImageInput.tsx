af57a0648fb46059bd83503502467dbcfbe22e69
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-10-31 14:57:15 -0500

    convert image input to preact component
    simplifies this quite a bit for not much JS weight

---
+++ src/components/forms/ImageInput.tsx
@@ -0,0 +1,72 @@
+import clsx from 'clsx'
+import { useEffect, useState } from 'preact/hooks'
+
+const imageTypes = ['image/gif', 'image/jpeg', 'image/png', 'image/webp']
+
+export default function ImageInput(props: {
+    name: string
+    required?: boolean
+}) {
+    const [hydrated, setHydrated] = useState(false)
+    useEffect(() => setHydrated(true), [])
+
+    const [previewUrl, setPreviewUrl] = useState<string>()
+
+    if (!hydrated) {
+        return (
+            <input
+                type="file"
+                name={props.name}
+                required={props.required}
+                accept={imageTypes.join(',')}
+            />
+        )
+    }
+
+    return (
+        <div
+            class={clsx(
+                'w-full max-w-md aspect-[16/9]',
+                'flex flex-col items-center justify-center gap-2',
+                'relative group',
+                'transition',
+                'focus:outline-none',
+                'bg-neutral-100 bg-center bg-cover hover:bg-neutral-200',
+                'text-neutral-600',
+                'border-2 border-dashed border-neutral-400 focus-within:border-primary-400',
+                'rounded-md overflow-hidden'
+            )}
+        >
+            <span class="text-2xl">
+                Preview image{' '}
+                {props.required && (
+                    <span
+                        class="text-red-500 text-base align-text-top"
+                        aria-hidden
+                    >
+                        *
+                    </span>
+                )}
+            </span>
+            <span>16:9, 906px wide or larger.</span>
+            {previewUrl && (
+                <img
+                    src={previewUrl}
+                    alt=""
+                    class="w-full h-full absolute inset-0 object-cover group-hover:opacity-25 transition"
+                />
+            )}
+            <input
+                type="file"
+                name={props.name}
+                required={props.required}
+                accept={imageTypes.join(',')}
+                class="opacity-0 absolute inset-0 cursor-pointer"
+                onInput={(event) => {
+                    const file = event.currentTarget.files[0]
+                    setPreviewUrl(file ? URL.createObjectURL(file) : undefined)
+                }}
+            />
+        </div>
+    )
+}
