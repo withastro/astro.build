57b4e0c70bfc43b05b36d6fb50ea5fd16ee7d2bd
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-11-02 15:38:11 -0500

    add fade-in to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -35,7 +35,16 @@ module.exports = {
                         }
                     }
                 }
-            })
+            }),
+            keyframes: {
+                'fade-in': {
+                    from: { opacity: 0 },
+                    to: { opacity: 1 }
+                }
+            },
+            animation: {
+                'fade-in': 'fade-in 0.5s ease-in-out'
+            }
         }
     },
     plugins: [
