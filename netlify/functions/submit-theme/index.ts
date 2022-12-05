92b402e95865ea51974f1523a05965d2685aaa28
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-12-05 14:41:41 -0600

    add a nice error page

--- netlify/functions/submit-theme/index.ts
+++ netlify/functions/submit-theme/index.ts
@@ -26,9 +26,18 @@ const redirect = (url: string): Response => ({
     statusCode: 303
 })
 
+const errorRedirect = (message: string): Response => ({
+    statusCode: 303,
+    headers: {
+        Location: `/themes/submit/error?errorMessage=${encodeURIComponent(
+            message
+        )}`
+    }
+})
+
 export const handler: Handler = async (event) => {
     if (!event.body) {
-        throw new Error('No body')
+        return errorRedirect('No body')
     }
 
     const form = await parseMultipartForm(
@@ -64,7 +73,9 @@ export const handler: Handler = async (event) => {
         body: initialMessageBody
     })
     if (!webhookResponse.ok) {
-        throw new Error(`Initial webhook failed: ${webhookResponse.statusText}`)
+        return errorRedirect(
+            `Initial webhook failed: ${webhookResponse.statusText}`
+        )
     }
 
     const webhookMessageJson = discordWebhookMessageSchema.parse(
@@ -79,12 +90,12 @@ export const handler: Handler = async (event) => {
     )
 
     if (!image) {
-        throw new Error('No image')
+        return errorRedirect('No image')
     }
 
     const themeName = getString('themeName')
     if (!themeName) {
-        throw new Error('Theme name required')
+        return errorRedirect('Theme name required')
     }
 
     const repoUrl = getString('repoUrl')
@@ -131,196 +142,10 @@ export const handler: Handler = async (event) => {
     )
 
     if (!discordResponse.ok) {
-        throw new Error(
+        return errorRedirect(
             `Message update failed failed: ${discordResponse.statusText}`
         )
     }
 
     return redirect('/themes/submit/success')
-
-    /* 
-    try {
-
-        const themeData: ThemeData = {
-            title: body.textFields.themeName?.[0] ?? '',
-            description: body.textFields.shortDescription?.[0] ?? '',
-            fullDescription: body.textFields.fullDescription?.[0] ?? ''
-        }
-
-        console.log(
-            'Theme data:',
-            inspect(themeData, { depth: Infinity, colors: true })
-        )
-
-        const branchName = `theme-submissions/${kebabCase(
-            themeData.themeName
-        )}-${now}`
-        console.log('Branch name:', branchName)
-
-        const themeFileName = `${kebabCase(themeData.themeName)}-${now}.json`
-        console.log('Theme file name:', themeFileName)
-
-        console.log('mkdir', path.dirname(repoFolder))
-        await fs.promises.mkdir(path.dirname(repoFolder), { recursive: true })
-
-        console.log('git.clone', repoUrl, repoFolder)
-        await git.clone({
-            fs,
-            http,
-            url: repoUrl,
-            dir: repoFolder,
-            singleBranch: true,
-            depth: 1
-        })
-
-        console.log('git.branch', branchName)
-        await git.branch({
-            fs,
-            dir: repoFolder,
-            ref: branchName
-        })
-
-        console.log('git.checkout', branchName)
-        await git.checkout({
-            fs,
-            dir: repoFolder,
-            ref: branchName
-        })
-
-        console.log(
-            'write',
-            path.join(repoFolder, 'src/data/themes', themeFileName)
-        )
-
-        const localThemeData: ThemeData = getLocalThemeData(themeData)
-
-        await fs.promises.writeFile(
-            path.join(repoFolder, 'src/data/themes', themeFileName),
-            JSON.stringify(localThemeData, undefined, 2)
-        )
-
-        console.log('git.add .')
-        await git.add({
-            fs,
-            dir: repoFolder,
-            filepath: path.join(repoFolder, 'src/data/themes', themeFileName)
-        })
-
-        console.log('git.commit')
-        await git.commit({
-            fs,
-            dir: repoFolder,
-            author: {
-                name: 'astrobot',
-                email: 'astrobot@astro.build'
-            },
-            message: `Add theme ${themeData.themeName}`
-        })
-
-        console.log('git.push')
-        await git.push({
-            fs,
-            http,
-            dir: repoFolder,
-            remote: 'origin',
-            ref: branchName,
-            onAuth: () => ({
-                username: 'astrobot',
-                password: process.env.GITHUB_TOKEN
-            })
-        })
-
-        console.log('create PR')
-        await octokit.pulls.create({
-            owner: 'withastro',
-            repo: 'astro.build',
-            title: `THEME: ${themeData.themeName ?? ''}`,
-            body: [
-                `**Author**: ${themeData.authorName ?? 'N/A'}`,
-                `**Paid or Free**: ${themeData.paidStatus ?? 'N/A'}`,
-                `**Repo Link**: ${themeData.repoUrl ?? 'N/A'}`,
-                `**Purchase Link**: ${themeData.purchaseUrl ?? 'N/A'}`,
-                `**Live Demo Link**: ${themeData.liveDemoUrl ?? 'N/A'}`,
-                `**Preview Image**: ${
-                    themeData.mainPreviewImage?.url ?? 'N/A'
-                }`,
-                ``,
-                themeData.shortDescription ?? ''
-            ].join('\n'),
-            head: branchName,
-            base: 'main'
-        })
-
-        console.log('done!')
-    } finally {
-        await fs.promises.rm(repoFolder, { recursive: true, force: true })
-    } */
 }
-/* 
-function getLocalThemeData(
-    themeData: z.infer<typeof themeDataSchema>
-): ThemeData {
-    const themeImages: Image[] = []
-    if (themeData.previewImage1) {
-        themeImages.push({
-            src: themeData.previewImage1.url,
-            alt: ''
-        })
-    }
-    if (themeData.previewImage2) {
-        themeImages.push({
-            src: themeData.previewImage2.url,
-            alt: ''
-        })
-    }
-    if (themeData.previewImage3) {
-        themeImages.push({
-            src: themeData.previewImage3.url,
-            alt: ''
-        })
-    }
-    if (themeData.previewImage4) {
-        themeImages.push({
-            src: themeData.previewImage4.url,
-            alt: ''
-        })
-    }
-
-    const links: Link[] = []
-    if (themeData.purchaseUrl) {
-        links.push({
-            href: themeData.purchaseUrl,
-            text: 'Purchase'
-        })
-    }
-
-    const localThemeData: ThemeData = {
-        title: themeData.themeName,
-        description: themeData.shortDescription,
-        fullDescription: '',
-        image: {
-            src: themeData.mainPreviewImage.url,
-            alt: `Preview for ${themeData.themeName}`
-        },
-        images: themeImages,
-        categories: [],
-        slug: ''
-    }
-
-    if (themeData.repoUrl) {
-        localThemeData.repoUrl = {
-            href: themeData.repoUrl,
-            text: 'View Repo'
-        }
-    }
-
-    if (themeData.liveDemoUrl) {
-        localThemeData.demoUrl = {
-            href: themeData.liveDemoUrl,
-            text: 'View Demo'
-        }
-    }
-
-    return localThemeData
-}
- */
