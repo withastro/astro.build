dd7bb6a59e332f2baac12f3a4e2fb0f206e3d932
bholmesdev <hey@bholmes.dev>
2023-01-13 15:07:10 -0500

    fix: move image mapping to getAuthor

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,6 @@
 import { defineCollection, z } from 'astro:content'
 import social from '../assets/social.png'
-import { authors, getAuthor, toJsonSlug } from '../data/authors'
-import { Person } from '../types'
+import { authors, toJsonSlug } from '../data/authors'
 
 type ArrayWithAtLeastOneEl = [string, ...string[]]
 const validAuthors = Object.keys(authors).map(
@@ -12,12 +11,11 @@ const blog = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        publishDate: z.string().transform((str) => new Date(str)),
-        authors: z
-            .array(z.enum(validAuthors))
-            .transform(async (authors): Promise<Person[]> => {
-                return authors.map((id) => getAuthor(id))
-            }),
+        publishDate: z
+            .string()
+            .or(z.date())
+            .transform((val) => new Date(val)),
+        authors: z.array(z.enum(validAuthors)),
         socialImage: z.string().optional().default(social.src),
         coverImage: z.string().optional(),
         lang: z.enum(['en']).default('en')
--- src/data/authors/index.ts
+++ src/data/authors/index.ts
@@ -1,5 +1,5 @@
 import { z } from 'astro:content'
-import { ImageSchema, Person } from '../../types.js'
+import { ImageSchema, Person, PersonSchema } from '../../types.js'
 
 export const authors = import.meta.glob('./*.json', { eager: true })
 export const images = import.meta.glob('./*{png,jpg,jpeg}', { eager: true })
@@ -14,16 +14,15 @@ function toJsonPath(key: string) {
     return `${jsonBase}${key}${jsonExt}`
 }
 
+const cache = new Map<string, Person>()
 export function getAuthor(id: string): Person {
+    if (cache.has(id)) return cache.get(id)!
+
     const authorMod = authors[toJsonPath(id)]
     if (!authorMod) throw new Error(`Author ${JSON.stringify(id)} not found.`)
-    const parsedAuthor = z
-        .object({
-            name: z.string(),
-            image: z.string(),
-            twitter: z.string().optional()
-        })
-        .safeParse(authorMod)
+    const parsedAuthor = PersonSchema.extend({ image: z.string() }).safeParse(
+        authorMod
+    )
     if (!parsedAuthor.success) {
         throw new Error(
             `Author ${JSON.stringify(id)} has invalid JSON. Full error: ${
@@ -32,8 +31,12 @@ export function getAuthor(id: string): Person {
         )
     }
     const author = parsedAuthor.data
+    const image = images[author.image] as { default: any }
+    if (!image) {
+        throw new Error(`Image ${JSON.stringify(author.image)} not found.`)
+    }
     const parsedImage = ImageSchema.safeParse({
-        src: (images[author.image] as any)?.default,
+        src: image.default,
         alt: author.name
     })
     if (!parsedImage.success) {
@@ -43,9 +46,11 @@ export function getAuthor(id: string): Person {
             }`
         )
     }
-    return {
+    const person: Person = {
         image: parsedImage.data,
         name: author.name,
         twitter: author.twitter
     }
+    cache.set(id, person)
+    return person
 }
