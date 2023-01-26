bd4ac8d185ee1117214769a36b766cc29409c09c
bholmesdev <hey@bholmes.dev>
2023-01-26 11:41:11 -0500

    chore: update RSS to latest version

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,35 +1,32 @@
-import rss from '@astrojs/rss';
-import type { MarkdownInstance } from 'astro';
-import { BlogPost } from '../types.js';
-import { slugFromFile } from './blog/_utils.js';
+import rss from '@astrojs/rss'
+import type { APIRoute } from 'astro'
+import { CollectionEntry, getCollection } from 'astro:content'
 
-function sortPosts(a, b) {
-    return Number(new Date(b.frontmatter.publishDate)) - Number(new Date(a.frontmatter.publishDate));
+function sortPosts(a: CollectionEntry<'blog'>, b: CollectionEntry<'blog'>) {
+    return Number(b.data.publishDate) - Number(a.data.publishDate)
 }
 
-function formatDate(dateStr: string) {
-    const date = new Date(dateStr);
-    date.setUTCHours(0);
-    return date;
+function formatDate(date: Date) {
+    date.setUTCHours(0)
+    return date
 }
 
-export const get = () => {
-    const allPosts: MarkdownInstance<BlogPost>[] = Object.values(import.meta.globEager('../content/blog/*.mdx'));
-
-    const sortedPosts = allPosts.sort((a, b) => sortPosts(a, b));
+export const get: APIRoute = async (context) => {
+    const unsortedPosts = await getCollection('blog')
+    const posts = unsortedPosts.sort((a, b) => sortPosts(a, b))
 
     return rss({
         // The RSS Feed title, description, and custom metadata.
         title: 'The Astro Blog',
         // See "Styling" section below
         description: 'News and updates about Astro.',
-        site: import.meta.env.SITE,
+        site: context.site!.href,
         // The list of items for your RSS feed, sorted.
-        items: sortedPosts.map(item => ({
-            title: item.frontmatter.title,
-            description: item.frontmatter.description,
-            link: `/blog/${slugFromFile(item.url)}`,
-            pubDate: formatDate(item.frontmatter.publishDate.toString()),
-        })),
-    });
+        items: posts.map((item) => ({
+            title: item.data.title,
+            description: item.data.description,
+            link: `/blog/${item.slug}/`,
+            pubDate: formatDate(item.data.publishDate)
+        }))
+    })
 }
