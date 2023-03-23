728440ead4966018df76bcb323ad9c4f6da989e7
Tony Sullivan <tony.f.sullivan@outlook.com>
2023-03-23 11:06:39 -0500

    adding Mastodon to the footer links, centralizing social link config

--- src/data/site-info.ts
+++ src/data/site-info.ts
@@ -3,6 +3,7 @@ export type SocialLink = {
 	href: string
 	me?: string
 	text: string
+	icon: string
 	footerOnly?: boolean
 }
 
@@ -32,17 +33,20 @@ const siteInfo: SiteInfo = {
 			href: "https://github.com/withastro/astro",
 			me: "https://github.com/withastro/",
 			text: "Go to Astro's GitHub repo",
+			icon: "social/github",
 		},
 		{
 			platform: "discord",
 			href: "/chat",
 			text: "Join the Astro community on Discord",
+			icon: "social/discord",
 		},
 		{
 			platform: "twitter",
 			href: "https://twitter.com/astrodotbuild",
 			me: "https://twitter.com/astrodotbuild",
 			text: "Follow Astro on Twitter",
+			icon: "social/twitter",
 		},
 		{
 			platform: "mastodon",
@@ -50,6 +54,7 @@ const siteInfo: SiteInfo = {
 			me: "https://m.webtoo.ls/@astro",
 			text: "Follow Astro on Mastodon",
 			footerOnly: true,
+			icon: "social/mastodon",
 		},
 	],
 }
