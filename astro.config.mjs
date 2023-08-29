862c4e17618f26b056793037d799694226cf3ab9
Tony Sullivan <15836226+tony-sull@users.noreply.github.com>
2023-08-29 18:43:51 +0000

    redirects should handle trailing slashes (#795)
    * testing trailingSlash config in deployed redirects
    
    * typo fix
    
    * tweaking the deprecated /play redirect
    
    * unrelated: handling local env for VERCEL_URL variable
    
    ---------
    
    Co-authored-by: Tony Sullivan <tony.f.sullivan@outlook.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,9 @@ import vercel from "@astrojs/vercel/serverless"
 
 /* https://vercel.com/docs/projects/environment-variables/system-environment-variables#system-environment-variables */
 const VERCEL_PREVIEW_SITE =
-	process.env.VERCEL_ENV !== "production" && `https://${process.env.VERCEL_URL}`
+	process.env.VERCEL_ENV !== "production" &&
+	process.env.VERCEL_URL &&
+	`https://${process.env.VERCEL_URL}`
 
 console.log("[VERCEL_PREVIEW_SITE]", VERCEL_PREVIEW_SITE)
 
