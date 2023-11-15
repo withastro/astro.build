ab028a644be8a431e12e9a1463cbc4f8e51c7da7
Nate Moore <natemoo-re@users.noreply.github.com>
2023-11-15 13:14:54 -0600

    Add API endpoint for integrations (#876)

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,1 +1,6 @@
 export type RequireSome<T, P extends keyof T> = Omit<T, P> & Required<Pick<T, P>>
+
+export type MapKeys<M extends Map<unknown, unknown>> = Prettify<Array<Parameters<M["get"]>[0]>>
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type Prettify<T> = { [K in keyof T]: T[K] } & {}
