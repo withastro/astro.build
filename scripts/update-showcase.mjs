0888a2a322d2dff0d93a2b59e9ae5b46e1f35c91
Chris Swithinbank <swithinbank@gmail.com>
2023-11-28 11:23:18 +0100

    Add blocked origin

--- scripts/update-showcase.mjs
+++ scripts/update-showcase.mjs
@@ -385,6 +385,7 @@ const scraper = new ShowcaseScraper({
 		"https://github.com",
 		"https://user-images.githubusercontent.com",
 		"https://camo.githubusercontent.com",
+		"https://private-user-images.githubusercontent.com",
 		"https://astro.build",
 		"https://pagespeed.web.dev",
 		"https://lighthouse-metrics.com",
