ed9815379eaba78ca2f354e215b958f18c508f46
Elian Van Cutsem <hello@elian.codes>
2024-03-14 21:15:11 +0100

    chore: remove old themes (content collections)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
-import { defineCollection } from "astro:content"
-import { z } from "zod"
+import { defineCollection } from "astro:content";
+import { z } from "zod";
 
 export const IntegrationCategories = new Map([
 	["featured", "Featured"],
@@ -12,81 +12,7 @@ export const IntegrationCategories = new Map([
 	["analytics", "Analytics"],
 	["accessibility", "Accessibility"],
 	["toolbar", "Dev Toolbar"],
-] as const)
-
-export const ThemeCategories = new Map([
-	["featured", "Featured"],
-	["recent", "Recently Added"],
-	["official", "Official"],
-	["blog", "Blog"],
-	["landing-page", "Landing Page"],
-	["portfolio", "Portfolio"],
-	["ecommerce", "E-commerce"],
-	["docs", "Docs"],
-	["minimal", "Minimal"],
-	["other", "Other"],
-] as const)
-
-export const ThemeTools = new Map([
-	["alpinejs", "Alpine.js"],
-	["lit", "Lit"],
-	["mdx", "MDX"],
-	["postcss", "PostCSS"],
-	["preact", "Preact"],
-	["react", "React"],
-	["sass", "SASS"],
-	["solidjs", "SolidJS"],
-	["svelte", "Svelte"],
-	["tailwind", "Tailwind"],
-	["unocss", "Unocss"],
-	["typescript", "TypeScript"],
-	["vue", "Vue"],
-] as const)
-
-export const ThemePricing = new Map<string, string>([
-	["free", "Free"],
-	["paid", "Paid"],
-])
-
-export const themeSchema = z
-	.object({
-		title: z.string().min(1),
-		description: z.string().min(1),
-		fullDescription: z.string().optional(),
-		image: z.string(),
-		images: z.array(z.string()).default([]),
-		author: z.object({
-			url: z.string(),
-			name: z.string(),
-			avatar: z.string(),
-		}),
-		categories: z.array(z.enum(Array.from(ThemeCategories.keys()) as [string, ...string[]])),
-		repoUrl: z.string().url().optional(),
-		demoUrl: z.string().url().optional(),
-		buyUrl: z.string().url().optional(),
-		links: z
-			.array(
-				z.object({
-					href: z.string().url(),
-					text: z.string(),
-				}),
-			)
-			.default([]),
-		stars: z.number().min(0).default(0),
-		featured: z.number().min(1).optional(),
-		tools: z.array(z.enum(Array.from(ThemeTools.keys()) as [string, ...string[]])).default([]),
-		related: z.array(z.string()).max(3).default([]),
-		publishDate: z.date({ coerce: true }).optional(),
-		badge: z.string().optional(),
-	})
-	.transform((theme) => {
-		// computed properties
-		return {
-			...theme,
-			official: theme.categories.includes("official"),
-			paid: !!theme.buyUrl,
-		}
-	})
+] as const);
 
 const seoSchema = z.object({
 	title: z.string().min(5).max(120),
@@ -104,7 +30,7 @@ const seoSchema = z.object({
 			follow: z.boolean().default(true),
 		})
 		.default({}),
-})
+});
 
 export const collections = {
 	authors: defineCollection({
@@ -153,14 +79,20 @@ export const collections = {
 	}),
 	integrations: {
 		schema: z.object({
-			name: z.string().describe("Name of the package as it is published to NPM"),
+			name: z
+				.string()
+				.describe("Name of the package as it is published to NPM"),
 			title: z
 				.string()
-				.describe("Title of the integration as it should be shown in the Integrations catalog"),
+				.describe(
+					"Title of the integration as it should be shown in the Integrations catalog",
+				),
 			description: z.string().optional(),
 			image: z.string().optional(),
 			categories: z.array(
-				z.enum(Array.from(IntegrationCategories.keys()) as [string, ...string[]]),
+				z.enum(
+					Array.from(IntegrationCategories.keys()) as [string, ...string[]],
+				),
 			),
 			repoUrl: z.string().url().optional(),
 			npmUrl: z.string().url(),
@@ -175,7 +107,9 @@ export const collections = {
 	pages: {
 		schema: z.object({
 			seo: seoSchema,
-			updated_date: z.date().describe("The date this content was last updated."),
+			updated_date: z
+				.date()
+				.describe("The date this content was last updated."),
 			locale: z.enum(["en"]).default("en"),
 		}),
 	},
@@ -204,7 +138,4 @@ export const collections = {
 			highlight: z.boolean().default(false),
 		}),
 	},
-	themes: {
-		schema: themeSchema,
-	},
-}
+};
