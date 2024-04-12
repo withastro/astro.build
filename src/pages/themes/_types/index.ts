727d4227923951811c3e722ec603855644dcf7e6
Elian Van Cutsem <hello@elian.codes>
2024-04-12 12:12:51 +0200

    fix: make DB fetched types typesafe

---
+++ src/pages/themes/_types/index.ts
@@ -0,0 +1,63 @@
+export type ThemeAndAuthor = {
+	Theme: Theme;
+	Author: Author;
+};
+
+export type Theme = {
+	id: number;
+	slug: string;
+	title: string;
+	featured: boolean;
+	description: string;
+	fullDescription: string;
+	body: string;
+	image: string;
+	images: Array<string>; // array of strings with URL's
+	authorId: number;
+	repoUrl?: string;
+	demoUrl?: string;
+	buyUrl?: string;
+	links: Array<string>; // array of strings with URL's
+	paid: boolean;
+	stars: number;
+	publishDate: Date;
+	updatedAt?: Date;
+	approved: boolean;
+	denied: boolean;
+	hidden: boolean;
+};
+
+export type ThemeHasCategory = {
+	themeId: number;
+	categoryId: number;
+};
+
+export type ThemeHasTool = {
+	themeId: number;
+	toolId: number;
+};
+
+export type Author = {
+	id: number;
+	url?: string;
+	name: string;
+	email?: string;
+	avatar?: string;
+	role: string;
+	githubId: number;
+	username: string;
+	updatedAt: Date;
+	createdAt: Date;
+};
+
+export type ThemeCategory = {
+	id: number;
+	value: string;
+	name: string;
+};
+
+export type ThemeTool = {
+	id: number;
+	value: string;
+	name: string;
+};
