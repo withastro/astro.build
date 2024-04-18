cbc49f98bce1cbdbab564697718654d4755eb9d4
Elian Van Cutsem <hello@elian.codes>
2024-04-18 12:59:26 +0100

    chore: add types to .env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,11 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+	readonly THEMES_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
