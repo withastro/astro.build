1ba46491c26a26249a89c8546800170d6437b2a6
Nate Moore <nate@astro.build>
2024-05-06 17:52:04 -0500

    update logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { defineMiddleware } from "astro:middleware";
 import { geolocation } from '@vercel/edge';
 
 export const onRequest = defineMiddleware(({ request, url }, next) => {
+    console.log('test', url.pathname)
     if (url.pathname !== '/') return next();
     const geo = geolocation(request);
     console.log(geo);
