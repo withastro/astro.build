bfd323fb4317bc4bbfc6b985721f2ed8ad14eb32
Nate Moore <nate@astro.build>
2024-05-06 17:48:08 -0500

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { defineMiddleware } from "astro:middleware";
 import { geolocation } from '@vercel/edge';
 
 export const onRequest = defineMiddleware(({ request, url }, next) => {
-    if (url.pathname !== '/') return;
+    if (url.pathname !== '/') return next();
     const geo = geolocation(request);
     console.log(geo);
     return next();
