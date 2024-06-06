a988818e17caf1425968d6e9e88b3630689d7ddc
Ian Mathaiya <iannmathaiya@gmail.com>
2024-06-06 20:34:31 +0300

    updated integrations code snippets to use mdx and shiki syntax highlighter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import db from "@astrojs/db";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import webVitals from "@astrojs/web-vitals";
-import astroExpressiveCode from "astro-expressive-code";
-import { defineConfig } from "astro/config";
 import houston from "./houston.theme.json";
+import { defineConfig } from "astro/config";
+import webVitals from "@astrojs/web-vitals";
+import vercel from "@astrojs/vercel/serverless";
+import astroExpressiveCode from "astro-expressive-code";
+
+import { rehypePrettyCode } from 'rehype-pretty-code';
 
 /* https://vercel.com/docs/projects/environment-variables/system-environment-variables#system-environment-variables */
 const VERCEL_PREVIEW_SITE =
@@ -18,6 +20,14 @@ const VERCEL_PREVIEW_SITE =
 // https://astro.build/config
 export default defineConfig({
 	site: VERCEL_PREVIEW_SITE || "https://astro.build",
+	markdown: {
+		syntaxHighlight: false,
+		rehypePlugins: [
+		  [
+			rehypePrettyCode, {}
+		  ],
+		],
+	  },
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
