41458920a661f5e481ecd48745e6e79d18e5e532
Fred K. Schott <fkschott@gmail.com>
2024-06-11 12:15:06 +0200

    update default container width

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -166,7 +166,7 @@ export default {
 				},
 
 				".container": {
-					"@apply w-full mx-auto max-w-screen-2xl px-4 md:px-8": {},
+					"@apply w-full mx-auto max-w-screen-xl px-4 md:px-8": {},
 				},
 
 				".grid-container": {
