fb71114f218c95b4fbc470eaaee8f6d61b462636
= <otterlord.dev@gmail.com>
2024-06-26 18:43:35 +0100

    Begin migration to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,41 @@
 import db from "@astrojs/db";
 import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import webVitals from "@astrojs/web-vitals";
 import astroExpressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
+import { rehypePrettyCode } from "rehype-pretty-code";
 import houston from "./houston.theme.json";
 
-import { rehypePrettyCode } from "rehype-pretty-code";
-
-/* https://vercel.com/docs/projects/environment-variables/system-environment-variables#system-environment-variables */
-const VERCEL_PREVIEW_SITE =
-	process.env.VERCEL_ENV !== "production" &&
-	process.env.VERCEL_URL &&
-	`https://${process.env.VERCEL_URL}`;
+/* https://docs.netlify.com/configure-builds/environment-variables/ */
+const NETLIFY_PREVIEW_SITE = process.env.CONTEXT !== "production" && process.env.DEPLOY_URL && `https://${process.env.DEPLOY_URL}`;
 
 // https://astro.build/config
 export default defineConfig({
-	site: VERCEL_PREVIEW_SITE || "https://astro.build",
-	markdown: {
-		syntaxHighlight: false,
-		rehypePlugins: [[rehypePrettyCode, {}]],
-	},
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		solid(),
-		astroExpressiveCode({
-			themes: [houston],
-			frames: false,
-		}),
-		mdx(),
-		sitemap(),
-		db(),
-		webVitals(),
-	],
-	image: {
-		domains: ["v1.screenshot.11ty.dev", "storage.googleapis.com"],
-	},
-	vite: {
-		ssr: {
-			noExternal: ["smartypants"],
-		},
-	},
-	output: "hybrid",
-	adapter: vercel({
-		imageService: true,
-		functionPerRoute: false,
-	}),
+  site: NETLIFY_PREVIEW_SITE || "https://astro.build",
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, {}]]
+  },
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), solid(), astroExpressiveCode({
+    themes: [houston],
+    frames: false
+  }), mdx(), sitemap(), db(), webVitals()],
+  image: {
+    domains: ["v1.screenshot.11ty.dev", "storage.googleapis.com"]
+  },
+  vite: {
+    ssr: {
+      noExternal: ["smartypants"]
+    }
+  },
+  output: "hybrid",
+  adapter: netlify({
+    imageCDN: true
+  })
 });
