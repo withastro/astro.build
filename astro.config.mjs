7ba71c38dfcae49822b66e1d5c551e17ec3b60fb
Chris Swithinbank <swithinbank@gmail.com>
2024-07-03 11:23:47 +0200

    `pnpm format`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import db from '@astrojs/db';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import solid from '@astrojs/solid-js';
-import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
-import webVitals from '@astrojs/web-vitals';
-import astroExpressiveCode from 'astro-expressive-code';
-import { defineConfig } from 'astro/config';
-import houston from './houston.theme.json';
+import db from "@astrojs/db";
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
+import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import webVitals from "@astrojs/web-vitals";
+import astroExpressiveCode from "astro-expressive-code";
+import { defineConfig } from "astro/config";
+import houston from "./houston.theme.json";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro.build',
+	site: "https://astro.build",
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
@@ -27,13 +27,13 @@ export default defineConfig({
 		webVitals(),
 	],
 	image: {
-		domains: ['v1.screenshot.11ty.dev', 'storage.googleapis.com'],
+		domains: ["v1.screenshot.11ty.dev", "storage.googleapis.com"],
 	},
 	vite: {
 		ssr: {
-			noExternal: ['smartypants'],
+			noExternal: ["smartypants"],
 		},
 	},
-	output: 'hybrid',
+	output: "hybrid",
 	adapter: netlify({ imageCDN: false }),
 });
