7b6f22113feb128c76dfc35806d242174fd2319d
Chris Swithinbank <swithinbank@gmail.com>
2024-08-23 23:06:47 +0200

    Account for search params and hash in trailing slash utility (#1229)

--- src/helpers/path.ts
+++ src/helpers/path.ts
@@ -1,2 +1,6 @@
 /** Adds a trailing `/` to the passed path if needed. */
-export const ensureTrailingSlash = (path: string) => (path.at(-1) === '/' ? path : `${path}/`);
+export const ensureTrailingSlash = (path: string) => {
+	const url = new URL(path, 'https://example.com');
+	if (url.pathname.at(-1) !== '/') url.pathname += '/';
+	return url.href.slice(url.origin.length);
+};
