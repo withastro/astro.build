a04ce2087a733d4a04aa2fdd5cffdcf0e48d730a
Chris Swithinbank <swithinbank@gmail.com>
2024-10-31 16:06:53 +0100

    Add `rel="nofollow ugc"` to links to external showcase sites and integrations (#1305)
    * Add `rel="nofollow"` to showcase and integration cards
    
    * Remove random extra attribute someone accidentally added
    
    * Revert "Remove random extra attribute someone accidentally added"
    
    This reverts commit d9032bf8494fba900d3325199a3a65d8d5fc883e.
    
    * Also add `ugc` to links
    
    * Add `rel="nofollow ugc"` to theme details pages
    
    * Add `rel="nofollow ugc"` to inline links in theme details
    
    Co-authored-by: ascorbic <213306+ascorbic@users.noreply.github.com>
    Co-authored-by: Princesseuh <3019731+Princesseuh@users.noreply.github.com>

--- src/helpers/themes.ts
+++ src/helpers/themes.ts
@@ -1,5 +1,12 @@
 import xss from 'xss';
 
+/** Matches `https://*.astro.build/*` and `https://github.com/withastro/*` */
+const officialDomainRE =
+	/^https:\/\/(?:(?:[^\/]+\.)?astro.build|github\.com\/withastro)(?:\/.*)?$/i;
+
+/** Extracts the value of a link’s `href` attribute to a named `href` group. */
+const hrefRE = /<a .*href="(?<href>[^"]+)".*>/i;
+
 export const sanitizeThemeDescription = (text: string) =>
 	xss(text, {
 		whiteList: {
@@ -17,5 +24,15 @@ export const sanitizeThemeDescription = (text: string) =>
 			h3: [],
 			a: ['href'],
 			span: [],
+		},
+		onTag(tag, html, options) {
+			if (tag !== 'a' || options.isClosing) return;
+			const matches = html.match(hrefRE);
+			if (matches?.groups?.href && officialDomainRE.test(matches.groups.href)) {
+				// Don’t modify links to official domains
+				return;
+			}
+			// Add `rel` attribute to unofficial links.
+			return html.replace(/>$/, ' rel="nofollow ugc">');
 		},
 	});
