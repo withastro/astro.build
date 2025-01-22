8f77cd99e25758f9db4750efd3797e8db6e71015
Chris Swithinbank <swithinbank@gmail.com>
2025-01-22 23:46:54 +0100

    Rework social links in site footer

--- src/data/site-info.ts
+++ src/data/site-info.ts
@@ -1,8 +1,12 @@
 export type SocialLink = {
 	me?: string;
+	/** Longer descriptive label, e.g. `"Join the Astro community on Discord"` */
 	text: string;
+	/** Short label with the name of the platform, e.g. `"Discord"`*/
+	label: string;
 	icon: string;
 	href: string;
+	/** Platform ID, e.g. `"discord"` */
 	platform: string;
 	footerOnly?: boolean;
 };
@@ -29,40 +33,36 @@ const siteInfo: SiteInfo = {
 	},
 	socialLinks: [
 		{
-			platform: 'discord',
-			footerOnly: true,
-			href: '/chat',
-			icon: 'social/discord',
-			text: 'Join the Astro community on Discord',
-		},
-		{
-			platform: 'twitter',
-			footerOnly: true,
-			icon: 'social/twitter',
-			me: 'https://x.com/astrodotbuild',
-			href: 'https://x.com/astrodotbuild',
-			text: 'Follow Astro on x.com (formerly Twitter)',
-		},
-		{
-			platform: 'github',
-			icon: 'social/github',
-			text: "Go to Astro's GitHub repo",
-			me: 'https://github.com/withastro/',
-			href: 'https://github.com/withastro/astro',
-		},
-		{
 			platform: 'bluesky',
 			footerOnly: true,
 			icon: 'social/bluesky',
 			me: 'https://bsky.app/profile/astro.build',
+			label: 'Bluesky',
 			text: 'Follow Astro on Bluesky',
 			href: 'https://bsky.app/profile/astro.build',
+		},
+		{
+			platform: 'discord',
+			footerOnly: true,
+			href: '/chat',
+			icon: 'social/discord',
+			label: 'Discord',
+			text: 'Join the Astro community on Discord',
+		},
+		{
+			platform: 'github',
+			icon: 'social/github',
+			label: 'GitHub',
+			text: "Go to Astro's GitHub repo",
+			me: 'https://github.com/withastro/',
+			href: 'https://github.com/withastro/astro',
 		},
 		{
 			platform: 'mastodon',
 			footerOnly: true,
 			icon: 'social/mastodon',
 			me: 'https://m.webtoo.ls/@astro',
+			label: 'Mastodon',
 			text: 'Follow Astro on Mastodon',
 			href: 'https://m.webtoo.ls/@astro',
 		},
@@ -71,8 +71,18 @@ const siteInfo: SiteInfo = {
 			footerOnly: true,
 			icon: 'social/reddit',
 			me: 'https://www.reddit.com/r/withastro/',
+			label: 'Reddit',
 			text: 'Join the official Astro community on Reddit',
 			href: 'https://www.reddit.com/r/withastro/',
+		},
+		{
+			platform: 'twitter',
+			footerOnly: true,
+			icon: 'social/twitter',
+			me: 'https://x.com/astrodotbuild',
+			href: 'https://x.com/astrodotbuild',
+			label: 'X.com',
+			text: 'Follow Astro on x.com (formerly Twitter)',
 		},
 	],
 };
