98329a495e99e31832e0493308c07d74f46f215b
Chris Swithinbank <swithinbank@gmail.com>
2026-03-16 22:21:19 +0100

    Cap retries at 5
    p-retry’s backoff retries 10 times by default which ends up with a final retry wait time of ~8m30s, which is probably excessive. Capping retries to 5 means we max out at a wait of 16s.

--- scripts/fetch.mjs
+++ scripts/fetch.mjs
@@ -10,21 +10,26 @@ const fetchLimit = pLimit(10);
  * @returns {Promise<Response>}
  */
 export function limitedFetch(url, init = {}) {
-	return pRetry(async (attempt) => {
-		return fetchLimit(async () => {
-			const res = await fetch(url, {
-				...init,
-				headers: { 'User-Agent': 'astro.build/integrations; v1', ...init.headers },
+	return pRetry(
+		async (attempt) => {
+			return fetchLimit(async () => {
+				const res = await fetch(url, {
+					...init,
+					headers: { 'User-Agent': 'astro.build/integrations; v1', ...init.headers },
+				});
+
+				// Return early for forbidden, not found, or method not allowed responses.
+				// These are unlikely to change with retries.
+				if (!res.ok && ![403, 404, 405].includes(res.status)) {
+					console.error(`[${url}] ${res.status} ${res.statusText} (Attempt ${attempt})`);
+					throw new Error();
+				}
+
+				return res;
 			});
-
-			// Return early for forbidden, not found, or method not allowed responses.
-			// These are unlikely to change with retries.
-			if (!res.ok && ![403, 404, 405].includes(res.status)) {
-				console.error(`[${url}] ${res.status} ${res.statusText} (Attempt ${attempt})`);
-				throw new Error();
-			}
-
-			return res;
-		});
-	});
+		},
+		{
+			retries: 5,
+		},
+	);
 }
